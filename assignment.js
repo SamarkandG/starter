acb5311fe9cd2408c624dce95cbcebea03c80510
SamarkandG <samarkand.guinot@gmail.com>
2022-07-22 00:24:43 +0200

    LECTURE : DATA TYPES

--- assignment.js
+++ assignment.js
@@ -3,3 +3,11 @@ let continent = "Europe";
 let population = "65 millions";
 
 console.log(country, continent, population);
+
+let isIsland = false;
+let language = "French";
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Samarkand");
@@ -6,3 +6,24 @@ console.log(23);
 
 let firstName = "Samarkand";
 console.log(firstName);
+
+//Je fais n'importe quoi dans les commentaires
+*/
+// let jsIsFun = true;
+// console.log(jsIsFun);
+
+// console.log(typeof true);
+// console.log(typeof jsIsFun);
+// console.log(typeof 12);
+// console.log(typeof "Sam");
+
+// jsIsFun = "YES!";
+// console.log(jsIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1995;
+// console.log(year);
+// console.log(typeof year);
