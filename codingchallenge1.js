01b97159c7553ccfa50a5291f5f903c94c96a007
SamarkandG <samarkand.guinot@gmail.com>
2022-07-22 23:45:34 +0200

    SAME

--- codingchallenge1.js
+++ codingchallenge1.js
@@ -40,9 +40,7 @@ const johnHeight = 1.76;
 const bmiMark = markMass / markHeight ** 2;
 const bmiJohn = johnMass / (johnHeight * johnHeight);
 
-const markHigherBMI = bmiMark > bmiJohn;
-
-if (markHigherBMI) {
+if (bmiMark > bmiJohn) {
   console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn}).`);
 } else {
   console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark}).`);
